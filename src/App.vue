8c2675a02ea805c7dfe18100d61303d479eb6d90
A.Rupesh <a.rupesh66@gmail.com>
2018-09-26 20:39:25 +0530

    final submission

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container" id="app">
+<img alt="Vue logo" src="./assets/logo.png">   
+    <SourceSelection v-on:sourceChanged="sourceChanged"></SourceSelection>
+    <Newslist v-bind:source="source"></Newslist>
+    
+    </div>
+</template>
+
+<script>
+import Newslist from './components/Newslist'
+import SourceSelection from './components/SourceSelection'
+
+export default {
+  name: 'app',
+  components: {
+    Newslist,
+    SourceSelection
+  },
+  data () {
+    return {
+      source: ""
+    }
+  },
+  methods: {
+    sourceChanged: function (source) {
+      this.source = source;
+    }
+  }
+}
+</script>
+
+<style>
+  #app {font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;  }
+  
+</style>
